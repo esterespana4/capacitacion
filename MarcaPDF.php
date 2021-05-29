6d53dc937e204e3493abef3a12a2638ad91427ba
esterespana4 <respana.1485@gmail.com>
2021-05-29 10:21:16 -0500

    version 1

---
+++ MarcaPDF.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php 
+
+$fecha_min="2020-08-01";
+$fecha_max="2021-08-31";
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+              <input id="fecha" name="fecha" type="date" size="70"  min="<?php echo $fecha_min;?>" max="<?php echo $fecha_max;?>"  />
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php 
+
+$fecha_min="2021-05-29";
+$fecha_max="2021-08-31";
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Rango de fechas</title>
+        <center><h2>Fecha Limitada</h2></center>
+    </head>
+    <body>
+        <center>
+            <img src="imagen/firma.jpg" width="184" height="157" alt="firma"/>
+            <input id="fecha" name="fecha" type="date" size="70"  min="<?php echo $fecha_min;?>" max="<?php echo $fecha_max;?>"  />
+        </center>
+    </body>
+        
+</html>
